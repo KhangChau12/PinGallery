75baebb8a35a992986fb39202b888c0232ce40a6
KhangChau12 <phuckhangtdn@gmail.com>
2025-03-16 23:21:39 +0700

    1

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -117,20 +117,3 @@ module.exports = {
     login,
     getCurrentUser
 };
-
-// routes/authRoutes.js - Authentication routes
-const express = require('express');
-const router = express.Router();
-const { register, login, getCurrentUser } = require('../controllers/authController');
-const auth = require('../middleware/auth');
-
-// Register route
-router.post('/register', register);
-
-// Login route
-router.post('/login', login);
-
-// Get current user route
-router.get('/me', auth, getCurrentUser);
-
-module.exports = router;
